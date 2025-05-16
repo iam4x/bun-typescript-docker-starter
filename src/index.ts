b0e6a5ee0af785a4fcaf3adc8514eac51bee34f9
iam4x <iam4x@me.com>
2025-05-16 15:02:50 +0400

    feat(index): add http server with health route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+
+serve({
+  port: process.env.PORT ?? 3000,
+  routes: {
+    "/health": new Response("OK"),
+    "/*": new Response("Not Found", { status: 404 }),
+  },
+});
