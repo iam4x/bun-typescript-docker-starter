86209317191ccb3120c9e7eb8ba4a4f91942cd52
iam4x <iam4x@me.com>
2026-03-30 14:36:31 +0400

    chore(tsconfig): more strict

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "bun";
 
 serve({
-  port: process.env.PORT ?? 3000,
+  port: process.env["PORT"] ?? 3000,
   routes: {
     "/health": new Response("OK"),
     "/*": new Response("Not Found", { status: 404 }),
